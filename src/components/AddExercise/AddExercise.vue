72b5f7a78df69b176574fcc265d869b885a29cc1
Jakomu <janne.komul@gmail.com>
2022-01-09 01:27:03 +0200

    end of the day, working on form logic

--- src/components/AddExercise/AddExercise.vue
+++ src/components/AddExercise/AddExercise.vue
@@ -1,10 +1,18 @@
 <template>
   <base-container>
-    <form>
+    <form @submit.prevent="submitExerciseForm">
       <h2>Add exercise:</h2>
       <div>
         <label for="sport">Choose your sport: </label>
-        <input type="text" name="sport" id="sport" />
+        <select name="sport" id="sport">
+          <option value="">Choose sport</option>
+          <option value="walk">walking</option>
+          <option value="run">running</option>
+          <option value="gym">gym</option>
+          <option value="games">games</option>
+          <option value="skiing">skiing</option>
+          <option value="other">other</option>
+        </select>
       </div>
       <div>
         <label for="date">Date: </label>
@@ -16,50 +24,79 @@
       </div>
       <div>
         <p>Add one or both:</p>
-        <label for="distance">Distance: </label>
-        <input type="number" name="distance" id="distance" /><br />
-        <label for="duration">Duration: </label>
+        <label for="distance">Distance (km): </label>
+        <input type="number" name="distance" id="distance" />
+        <br />
+        <label for="duration">Duration (min): </label>
         <input type="number" name="duration" id="duration" />
       </div>
       <label for="feeling">Feeling:</label>
       <div class="feelings">
         <span>
           <label for="vgood"></label>
-          <input type="radio" name="vgood" id="vgood" value="5" />
+          <input type="radio" name="feeling" id="vgood" value="5" />
           <img src="" alt="5" />
         </span>
         <span>
           <label for="good"></label>
-          <input type="radio" name="good" id="good" value="4" />
+          <input type="radio" name="feeling" id="good" value="4" />
           <img src="" alt="4" />
         </span>
         <span>
           <label for="semi"></label>
-          <input type="radio" name="semi" id="semi" value="3" />
+          <input type="radio" name="feeling" id="semi" value="3" />
           <img src="" alt="3" />
         </span>
         <span>
           <label for="bad"></label>
-          <input type="radio" name="bad" id="bad" value="2" />
+          <input type="radio" name="feeling" id="bad" value="2" />
           <img src="" alt="2" />
         </span>
         <span>
           <label for="vbad"></label>
-          <input type="radio" name="vbad" id="vbad" value="1" />
+          <input type="radio" name="feeling" id="vbad" value="1" />
           <img src="" alt="1" />
         </span>
+      </div>
+      <div>
+        <label for="notes">Notes:</label><br />
+        <textarea name="notes" id="notes" cols="30" rows="3"></textarea>
       </div>
       <base-button>Add exercise</base-button>
     </form>
   </base-container>
 </template>
 
+<script>
+import { mapActions } from "vuex";
+export default {
+  methods: {
+    submitExerciseForm(event) {
+      console.log(event.target.time.value);
+      const data = {
+        sport: event.target.sport.value,
+        date: event.target.date.value,
+        time: event.target.time.value,
+        distance: event.target.distance.value,
+        duration: event.target.duration.value,
+        feeling: event.target.feeling.value,
+        notes: event.target.notes.value,
+      };
+      this.submitExercise(data);
+    },
+  },
+  computed: {
+    ...mapActions(["submitExercise"]),
+  },
+};
+</script>
+
 <style scoped>
 div {
-  margin: 0.5rem;
+  margin: 0.25rem;
 }
 input {
-  margin: 0.5rem;
+  margin: 0.25rem;
 }
 span {
   display: flex;
@@ -69,5 +106,8 @@ span {
   display: flex;
   flex-direction: row;
   justify-content: center;
+}
+p {
+  padding: 0.25rem;
 }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,26 @@ import { createStore } from "vuex";
 
 const store = createStore({
   state() {
-    return {};
+    return {
+      sport: "",
+      date: "",
+      time: "",
+      distance: 0,
+      duration: 0,
+      feeling: 0,
+      notes: "",
+    };
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    submitExercise(payload) {
+      console.log(payload);
+    },
+  },
+  actions: {
+    submitExercise(context, payload) {
+      context.commit("submitExercise", payload);
+    },
+  },
 });
 
 export default store;
