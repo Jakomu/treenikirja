993442ffb98dff14b9b49bb8c1c5dba78963b462
Jakomu <janne.komul@gmail.com>
2022-01-09 18:36:01 +0200

    added welcome text

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,3 +1,20 @@
 <template>
-  <p>teretulemas</p>
+  <h1>Welcome</h1>
+  <p>
+    Here you can log in your exercises, so you know what you have been doing!
+  </p>
+  <p>You know: <span> Log it, or it didn't happen!</span></p>
 </template>
+
+<style scoped>
+h1 {
+  margin: 3rem;
+}
+p {
+  margin: 2rem;
+  font-weight: bold;
+}
+span {
+  font-size: large;
+}
+</style>
